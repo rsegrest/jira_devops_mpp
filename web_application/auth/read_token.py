a15ef620517bc21f657a419d8e2478d0616a26b0
Rick Segrest <4419504+rsegrest@users.noreply.github.com>
2023-11-02 22:35:26 +0000

    Instructions about how to add token to web_application

--- web_application/auth/read_token.py
+++ web_application/auth/read_token.py
@@ -1,5 +1,7 @@
 def read_token():
-    """ Read token from text file, return token string """
+    """ Read token from text file, return token string
+        Add your token to a file called 'token.txt' in this directory
+    """
     with open('auth/token.txt', 'r', encoding='utf-8') as file:
         token = file.read()
     return token
